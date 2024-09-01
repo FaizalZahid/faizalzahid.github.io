6b80d889c99ddf9635983e693ed8f7c111a1645e
FaizalZahid <manifestasisanubari@gmail.com>
2024-09-01 14:13:08 +0800

    Add files via upload

--- free-ebooks/script.js
+++ free-ebooks/script.js
@@ -8,29 +8,35 @@ document.addEventListener("DOMContentLoaded", function() {
         while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
 
         links.forEach((link) => link.classList.remove("active"));
-        if (links[index]) {
-            links[index].classList.add("active");
-        }
+        links[index].classList.add("active");
     }
 
     activateLink();
     window.addEventListener("scroll", activateLink);
 
     links.forEach((link) => {
-        const href = link.getAttribute("href");
-        if (href && href.startsWith('#')) {
-            link.addEventListener("click", function(e) {
-                e.preventDefault();
-                const targetId = this.getAttribute("href").substring(1);
-                const targetSection = document.getElementById(targetId);
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
 
-                if (targetSection) {
-                    window.scrollTo({
-                        top: targetSection.offsetTop,
-                        behavior: "smooth",
-                    });
-                }
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: "smooth",
             });
-        }     
+        });
     });
+});
+
+document.querySelectorAll('.toggle-button').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    if (content.style.display === "none" || !content.style.display) {
+      content.style.display = "block";
+      button.classList.add('active');
+    } else {
+      content.style.display = "none";
+      button.classList.remove('active');
+    }
+  });
 });
