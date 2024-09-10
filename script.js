5d46c7fa65a8baad7c016befa5fd27d7eb2bab2e
FaizalZahid <manifestasisanubari@gmail.com>
2024-09-10 09:30:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -37,4 +37,15 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }     
     });
+
+    document.querySelectorAll('.toggle-button').forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
+            if (content.style.display === "none" || !content.style.display) {
+                content.style.display = "block";
+            } else {
+                content.style.display = "none";
+            }
+        });
+    });
 });
